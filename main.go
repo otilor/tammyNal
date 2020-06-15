7c723a88a75a0b0de1f5e935fc3f6bc359cdb7d6
Gabriel Akinyosoye <gabrielfemi799>
2020-06-15 13:23:21 -0400

    Include package tammyNal
    Took 3 minutes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/GabielFemi/tammyNal/tammyNal"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter
+	router().HandleFunc("/", tammyNal.Index)
 }
